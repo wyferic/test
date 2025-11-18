739a8897dbaa7bc20cbb401a96a5d601446f24eb
wyferic <yi.feng.eric.wang@gmail.com>
2025-11-17 23:09:51 -0500

    does the node env thing even work?

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
+
 const nextConfig = {
-  basePath: "/test",
+  basePath: isProd ? '/test' : '',
   output: "export", 
   reactStrictMode: true,
 };
