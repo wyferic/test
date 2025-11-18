ea8f941a03fabe8c05910b60321324cb0a842d95
wyferic <yi.feng.eric.wang@gmail.com>
2025-11-17 19:39:23 -0500

    bruh

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
->>>>>>> 4fd034ad94964a9c6eda8bbd5a96fb6406028101
 };
 
 export default nextConfig;
